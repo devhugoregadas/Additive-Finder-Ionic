2a43dea3a65bdb7dc0d20de384c2943a71b146a0
Hugo Regadas <dev.hugoregadas@gmail.com>
2022-02-28 20:12:52 +0100

    Components & Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { TabBarComponent } from './shared/components/tab-bar/tab-bar.component';
+import { Page1Component } from './features/containers/page1/page1/page1.component';
+import { Page2Component } from './features/containers/page2/page2.component';
+import { Page3Component } from './features/containers/page3/page3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabBarComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component
   ],
   imports: [
     BrowserModule,
